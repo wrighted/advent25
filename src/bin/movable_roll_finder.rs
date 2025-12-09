2a99f8742146e7e4e7b5c89d22aff2bbdb6446bc
Ethan Wright <wrighttd@amazon.com>
2025-12-09 00:42:30 +0000

    Day 4 Part 2

--- src/bin/movable_roll_finder.rs
+++ src/bin/movable_roll_finder.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::env;
 
+#[derive(Clone)]
 struct Spot {
     here: bool
 }
@@ -15,12 +16,17 @@ impl Spot {
     }
 }
 
+#[derive(Clone)]
 struct Grid {
     grid: Vec<Vec<Spot>>
 }
 
 impl Grid {
     fn num_accessible(&self) -> i32 {
+        self.get_accessible().len() as i32
+    }
+
+    fn get_accessible(&self) -> Vec<(i32, i32)> {
         (0..self.grid.len())
         .flat_map(|i| (0..self.grid[i].len()).map(move |j| (i, j)))
         .filter(|&(i, j)| self.grid[i][j].is_roll())
@@ -30,7 +36,8 @@ impl Grid {
                 .map(|&(di, dj)| self.is_full(i as i32 + di, j as i32 + dj))
                 .sum::<i32>() < 4
         })
-        .count() as i32
+        .map(|(i, j)| (i as i32, j as i32))
+        .collect()
     }
 
     fn is_full(&self, i: i32, j: i32) -> i32 {
@@ -39,6 +46,25 @@ impl Grid {
             && (j as usize) < self.grid[i as usize].len() 
             && self.grid[i as usize][j as usize].is_roll()) as i32
     }
+
+    fn remove_spots(&self, spots: &Vec<(i32, i32)>) -> Grid {
+        let mut new_grid = self.grid.clone();
+        for &(i, j) in spots {
+            new_grid[i as usize][j as usize] = Spot { here: false };
+        }
+        Grid { grid: new_grid }
+    }
+}
+
+fn elves(grid: Grid, accessed: i32) -> i32 {
+    if grid.num_accessible() == 0 {
+        return accessed;
+    }
+
+    let spots = grid.get_accessible();
+    let new_grid = grid.remove_spots(&spots);
+
+    elves(new_grid, accessed + spots.len() as i32)
 }
 
 fn main() {
@@ -51,5 +77,8 @@ fn main() {
             .collect()
         };
 
-    print!("{}", grid.num_accessible());
+    let p1 = grid.num_accessible();
+    let p2 = elves(grid.clone(), 0);
+
+    print!("p1: {}, p2: {}", p1, p2);
 }
